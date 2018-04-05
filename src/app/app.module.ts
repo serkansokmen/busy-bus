e4a67b9a95dfbdcd242c6d86c3ce8e8be375b7fc
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2018-04-06 02:36:50 +0300

    updated outdated packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule, MatIconModule, MatDialogModule } from '@angular/material';
-import { ShareModule } from 'ng2share/share.module';
+import { ShareModule } from '@ngx-share/core';
 import { AppComponent } from './app.component';
 import { TetrisComponent } from './tetris/tetris.component';
 import { GameOverDialog } from './game-over-dialog/game-over-dialog.component';
@@ -13,10 +14,11 @@ import { WelcomeComponent } from './welcome/welcome.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     MatButtonModule,
     MatIconModule,
     MatDialogModule,
-    ShareModule,
+    ShareModule.forRoot(),
   ],
   declarations: [
     AppComponent,
