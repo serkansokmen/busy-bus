a28eacc0cdcbfc44715f5985fae84b8520f1fd9b
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2018-07-12 19:17:43 +0300

    added retina support for game canvas

--- src/app/components/tetris/tetris.component.ts
+++ src/app/components/tetris/tetris.component.ts
@@ -87,15 +87,20 @@ export class TetrisComponent implements OnInit, AfterViewInit, OnDestroy {
     this.context.mozImageSmoothingEnabled = true;
     this.context.webkitImageSmoothingEnabled = true;
     this.context.mozImageSmoothingEnabled = true;
-    this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
   }
 
   ngAfterViewInit() {
+    this.clearCanvas();
+    this.scaleCanvasForRetina(this.canvas.nativeElement, this.context, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
     this.startNewGame();
   }
 
   ngOnDestroy() {
     cancelAnimationFrame(this.animationFrameRequestId);
+  }
+
+  private clearCanvas() {
+    this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
   }
 
   startNewGame() {
@@ -303,7 +308,7 @@ export class TetrisComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private draw() {
-    this.context.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+    this.clearCanvas();
 
     this.drawMatrix(this.context, this.arena, {x: 0, y: 0});
     this.drawMatrix(this.context, this.player.matrix, this.player.pos);
@@ -406,4 +411,41 @@ export class TetrisComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   private random(min, max) { return (min + (Math.random() * (max - min))); }
+
+  private scaleCanvasForRetina(canvas, context, width, height) {
+    // assume the device pixel ratio is 1 if the browser doesn't specify it
+    const devicePixelRatio = window.devicePixelRatio || 1;
+
+    // determine the 'backing store ratio' of the canvas context
+    const backingStoreRatio = (
+      context.webkitBackingStorePixelRatio ||
+      context.mozBackingStorePixelRatio ||
+      context.msBackingStorePixelRatio ||
+      context.oBackingStorePixelRatio ||
+      context.backingStorePixelRatio || 1
+    );
+
+    // determine the actual ratio we want to draw at
+    const ratio = devicePixelRatio / backingStoreRatio;
+
+    if (devicePixelRatio !== backingStoreRatio) {
+      // set the 'real' canvas size to the higher width/height
+      canvas.width = width * ratio;
+      canvas.height = height * ratio;
+
+      // ...then scale it back down with CSS
+      canvas.style.width = width + 'px';
+      canvas.style.height = height + 'px';
+    }
+    else {
+      // this is a normal 1:1 device; just scale it simply
+      canvas.width = width;
+      canvas.height = height;
+      canvas.style.width = '';
+      canvas.style.height = '';
+    }
+
+    // scale the drawing context so everything will work at the higher ratio
+    context.scale(ratio, ratio);
+  }
 }
